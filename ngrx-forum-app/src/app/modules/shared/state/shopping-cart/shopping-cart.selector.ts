9674f7bed56866f0c5e9ca34014b5686088c1cad
Louis van Zyl <louisvanzyl@outlook.com>
2024-10-10 20:03:54 +0200

    Set selectors

---
+++ ngrx-forum-app/src/app/modules/shared/state/shopping-cart/shopping-cart.selector.ts
@@ -0,0 +1,17 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { CartState } from './shopping-cart.state';
+import { appCartStateFeatureKey } from './shopping-cart.reducer';
+
+const selectCartFeature = createFeatureSelector<CartState>(
+  appCartStateFeatureKey
+);
+
+export const selectCartItems = createSelector(
+  selectCartFeature,
+  (state: CartState) => state.items
+);
+
+export const selectCartConfig = createSelector(
+  selectCartFeature,
+  (state: CartState) => state.cartConfig
+);
